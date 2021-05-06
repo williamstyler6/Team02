5d33e9cb950c1820d367855ef10a79824219d245
rhenriquea <rafa.almeida.js@gmail.com>
2021-05-06 18:40:38 +0200

    week 03 solution

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,14 +1,47 @@
-//TA03 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
-router.get('/',(req, res, next) => {
+let ALL_PRODUCTS = [];
+
+router.get('/', async (req, res, next) => {
+
+    const BASE_URL = 'https://byui-cse.github.io/cse341-course/lesson03';
+    const items = 'items.json';
+
+    try {
+        const { data } = await axios.get(`${BASE_URL}/${items}`);
+        ALL_PRODUCTS = data;
+    } catch (error) {
+        ALL_PRODUCTS = [];
+        throw new Error('Something went wrong');
+    }
+
+
     res.render('pages/ta03', { 
         title: 'Team Activity 03', 
-        path: '/ta03', // For pug, EJS 
-        activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
+        path: '/ta03',
+        totalProducts: ALL_PRODUCTS.length,
+        products: ALL_PRODUCTS,
     });
+});
+
+router.post('/', async (req, res, next) => {
+    const { search } = req.body;
+
+    if(!search) {
+        res.redirect('/ta03');
+    }
+
+    const filteredProducts = ALL_PRODUCTS.filter(p => p.name.toLowerCase().includes(search.toLowerCase()));
+    
+    res.render('pages/ta03', { 
+        title: 'Team Activity 03', 
+        path: '/ta03',
+        totalProducts: ALL_PRODUCTS.length,
+        products: filteredProducts,
+    });
+
 });
 
 module.exports = router;
